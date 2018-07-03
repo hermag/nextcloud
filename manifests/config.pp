ce22a672d80ab45c86583268ca5eb3542e9f00b0
Erekle Magradze <emagradze@targamos.herkulanum.net>
2018-07-03 12:55:01 +0200

    adding content

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,39 +1,18 @@
 include '::mysql::server'
 
  class nextcloud::config inherits nextcloud {
-    # if $::nextcloud::manage_db {
-    #    class { 'mysql::server':
-    #       root_password           => $dbrootpassword,
-    #       remove_default_accounts => true,
-    #    }
-    # }
+    if $::nextcloud::manage_db {
+        class { '::mysql::server':
+                   root_password => $dbrootpassword,
+                   remove_default_accounts => true,
+                   #override_options => $override_options
+              }
 
-    # $mysql_password = "myT0pS3cretPa55worD"
-    # exec { "set-mariadb-password":
-    #     unless => "mysqladmin -uroot -p$mysql_password status",
-    #     path => ["/bin", "/usr/bin"],
-    #     command => "mysqladmin -uroot password $mysql_password",
-    #     require => Service["mariadb"],
-    # }
-
-    #$user = "nxtuser"
-    #$password = "Nextcloud@123#"
-    #$dbname = "nextcloud_db"
-    #exec { $dbname:
-    #    unless => "/usr/bin/mysql -u${user} -p${password} ${dbname}",
-    #    command => "/usr/bin/mysql -uroot -p$mysql_password -e \"create database ${dbname}; grant all on ${dbname}.* to ${user}@localhost identified by '$password';\"",
-    #    require => Exec["set-mariadb-password"],
-    #}
-    class { '::mysql::server':
-               root_password => $dbrootpassword,
-               remove_default_accounts => true,
-               #override_options => $override_options
-          }
-
-    mysql::db { $dbname:
-          user     => $dbuser,
-          password => $dbpass,
-          host     => 'localhost',
-          grant    => ['ALL'],
-         }
+        mysql::db { $dbname:
+              user     => $dbuser,
+              password => $dbpass,
+              host     => 'localhost',
+              grant    => ['ALL'],
+             }
+    }
 }
