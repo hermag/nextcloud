b76ec8137a7726af8c2b211002b3abf8b2430d3c
Erekle Magradze <emagradze@targamos.herkulanum.net>
2018-07-03 15:06:50 +0200

    adding content

--- manifests/config.pp
+++ manifests/config.pp
@@ -29,17 +29,17 @@ include '::mysql::server'
     file {
            "$keyroot/apache-selfsigned.crt.erb":
               content => template('nextcloud/apache-selfsigned.crt.erb'),
-              owner   => root,
-              group   => root,
-              mode    => 644,
+              owner   => "root",
+              group   => "root",
+              mode    => "644",
     }
 
     file {
            "$certroot/apache-selfsigned.crt.erb":
               content => template('nextcloud/apache-selfsigned.key.erb'),
-              owner   => root,
-              group   => root,
-              mode    => 644,
+              owner   => "root",
+              group   => "root",
+              mode    => "644",
     }
 
 }
